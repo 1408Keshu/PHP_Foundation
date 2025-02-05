54a0b5ea055629fd43ddc630073746accbd3ba53
keshav31 <keshavchauhangtx2004@gmail.com>
2025-02-05 20:23:58 +0530

    variable in php

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
 <?php
-echo "Hello World";
+echo "Hello World <br>";
+echo "kandi undi";
+// this is one line comments
+/*
+echo "
+this 
+is multi 
+line
+ comment
+*/
+
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <button>More</button>
+</body>
+</html>
---
+++ variable.php
@@ -0,0 +1,32 @@
+<?php
+    $name= "Keshav";
+    $food =  "Pizza";
+    $email  = "keshav@gmail.com";
+
+    $age = 21 ;
+    $quantity = 3;
+    $user = 4;
+
+    $Cgpa = 7.7;
+    $price = 5.5;
+    $tax_ratio =  5.2;
+
+    $student = true;
+    $employee = false;
+    $adult = true;
+
+    $total = null;
+
+
+    echo "Hello {$name} <br>";
+    echo "My Favourite food is  {$food} <br>";
+    echo "Contact me on {$email} <br>";
+    echo "My Age is {$age} <br>";
+    echo "Here is my Cgpa {$Cgpa} <br>";
+    echo "Price is \${$price}<br>";
+    echo "Am i Student {$student}<br>";
+    echo "Am i Employee {$student}<br>";
+    echo "Am i adult {$student}<br>";
+    echo "toatl is \${$total}<br>";
+    
+?>
