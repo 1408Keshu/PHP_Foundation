4e08815ef7b60777a0072667bb2e977e9822a6f4
keshav31 <keshavchauhangtx2004@gmail.com>
2025-02-06 10:12:28 +0530

    Arithmetic Operations in php

---
+++ Arithmetic.php
@@ -0,0 +1,33 @@
+<?php
+
+    $a= 3;
+    $b= 4;
+    $c= 6;
+
+    // $z= $a +$b;
+    // $z= $a - $b;
+    // $z= $a * $b;
+    // $z= $a / $b;
+    
+    
+    // $z= $a**$b;
+    // $z= $a%$b;
+
+    //Increment/Decrement
+    // $counter = 0;
+    // $counter = $counter + 1;
+    // $counter +=  1;
+    // $counter++;
+    // $counter = $counter - 1;
+    // $counter -=  1;
+    // $counter--;
+    
+    //Operator Precedence
+    $total = 1+2-3*4/5**(6+3);
+    
+
+
+echo $total;
+
+
+?>
