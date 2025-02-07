60b37e408325ba1a858aeee7d62a8a8454736c7e
keshav31 <keshavchauhangtx2004@gmail.com>
2025-02-07 11:07:36 +0530

    Logical Operator

---
+++ logicalOperator.php
@@ -0,0 +1,33 @@
+<?php
+    // $temp =0;
+    // $employee = false;
+    // $homeless=true;
+    // if($temp>30 && $temp<20){
+    //     echo "Its Warm Outside";
+    // }
+    // else if( $temp> 30 && $temp< 60){
+    //     echo "Its Hot outside";
+    // }
+    // else if( $temp< 20 && $temp> -50){
+    //     echo "Its cold Outside";
+    // }
+    //     else{
+    //         echo "Its unbearable";
+    // }
+
+    // if(!$employee){
+    //     echo "He is an unemployed";
+    // }
+    // if( $temp< 0 || $homeless ){
+    //     echo " man Freezing outside";
+    // }
+
+    $age = 21;
+    $citizen = false;
+    if($age>18 && $citizen){
+        echo "You can vote";
+    }
+    else{
+        echo "You cannot vote";
+    }
+?>
