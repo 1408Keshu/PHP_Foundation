a448f070c677516077ebe79a48b98e24a7cbaa86
keshav31 <keshavchauhangtx2004@gmail.com>
2025-02-08 22:11:16 +0530

    while loop in php

---
+++ while.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<form action="while.php" method="post">
+    <!-- <input type="text" name="number" placeholder="Enter a number"> -->
+    <input type="submit">
+</form>
+<body>
+    
+</body>
+</html>
+<?php
+
+    // $number = $_POST["number"];
+    // $counter = 0;
+    // while($counter <$number){
+    //     $counter++;
+    //     echo $number*$counter."<br>";
+    // }
+
+    $running =true;
+    while($running){
+        if(isset($_POST)){
+            
+        }
+    }
+
+?>
