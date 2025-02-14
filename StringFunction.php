d0014811fad4a4cf043c4f24b8cfcfb75ebf3b10
keshav31 <keshavchauhangtx2004@gmail.com>
2025-02-14 10:45:44 +0530

    using String functions in php

---
+++ StringFunction.php
@@ -0,0 +1,37 @@
+<?php 
+    // $username = "hello Bro code ";
+    $username = array("hello", "Bro"," code ");
+    $phone = "123-456-7890";
+    // $username = strtolower($username);
+    // $username = strtoupper($username);
+    // $username = trim($username);
+    // $username = str_pad($username,10 ,"0");
+    // $phone = str_replace("-"," | ",$phone);
+    // $username = strrev($username);
+    // $username = str_shuffle($username);
+    // $phone = str_shuffle($phone);
+    // $equals = strcmp($username,"Bro Code");
+    // $count =strlen($phone);
+    // $index = strpos($username,"c");
+    // $firstname = substr($username,0,3);
+    // $lastname = substr($username,4);
+    // $fullname = explode(" ",$username);
+    $username = implode("-",$username);
+
+
+
+
+    
+    // echo $username."<br>";
+    // echo $phone;
+    // echo $equals;
+    // echo $count;
+    // echo $index;
+    // echo $firstname;
+    // echo $lastname;
+    // foreach($fullname as $name){
+    //     echo $name."<br>";
+    // }
+    echo $username;
+    
+?>
