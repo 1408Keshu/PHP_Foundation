d98731f80a5e8a34607d6692e6dbc91618b445eb
Keshav Chauahan <keshavchauhan1312@gmail.com>
2025-02-22 12:39:57 +0530

    php insert into my sql database

--- PHPWithDatabase/index.php
+++ PHPWithDatabase/index.php
@@ -1,7 +1,22 @@
 <?php
     include("database.php");
+
+    $username = "keshava";
+    $password = "keshava1231";
+    $hash = password_hash($password,PASSWORD_DEFAULT);
+    // $sql = "INSERT INTO users (username, password) VALUES ('$username','$password')";
+    $sql = "INSERT INTO users (username ,password) VALUES ('$username','$hash')";
+    // $sql = "INSERT INTO users (username, password ) VALUES ('shubham','shubhu121')";
+    try{
+        // mysqli_query($conn,$sql);
+        echo "<br>"."User is now registered";
+    }
+    catch(mysqli_sql_exception){
+        echo "Could not registered user";
+    }
+    // mysqli_close($conn);
 ?>
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -11,4 +26,4 @@
 <body>
     <br>Hello 
 </body>
-</html>
+</html> -->
